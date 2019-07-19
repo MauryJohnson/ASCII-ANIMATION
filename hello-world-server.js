f497c726c710b8ce6c988ad1ed3834367f2b70d3
maury johnson <mauryljohnsonii@gmail.com>
2019-07-18 23:14:15 -0400

    First

---
+++ hello-world-server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    
+	
+	if(req.method==="GET" && req.url.indexOf("Example")!=-1){
+	//res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile("./"+req.url, function(err, data) {
+    if(err)
+    	throw err;
+    	
+    var dotoffset = req.url.lastIndexOf('.');
+    var mimetype = dotoffset == -1
+                    ? 'text/plain'
+                    : {
+                        '.html' : 'text/html',
+                        '.ico' : 'image/x-icon',
+                        '.jpg' : 'image/jpeg',
+                        '.png' : 'image/png',
+                        '.gif' : 'image/gif',
+                        '.css' : 'text/css',
+                        '.js' : 'text/javascript'
+                        }[ req.url.substr(dotoffset) ];
+    res.setHeader('Content-type' , mimetype);
+
+    res.end(data);
+    
+	});
+    
+	}
+	
+}).listen(1335, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
