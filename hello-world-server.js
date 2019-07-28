191da229d1da789296200441f3a05e7107a2e983
maury johnson <mauryljohnsonii@gmail.com>
2019-07-27 22:14:47 -0400

    Fix clipping bug

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var PORT = 1337;
 
 http.createServer(function (req, res) {
     
@@ -30,5 +31,5 @@ http.createServer(function (req, res) {
     
 	}
 	
-}).listen(1330, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(PORT, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:'+PORT);
